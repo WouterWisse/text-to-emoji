8542cc759c9648baaaa7c74b0151e16defc63e51
Wouter Wisse <wouter.wisse@persgroep.net>
2022-08-10 17:04:42 -0400

    Remove whitespace

--- Sources/TextToEmoji/TextToEmoji.swift
+++ Sources/TextToEmoji/TextToEmoji.swift
@@ -5,9 +5,7 @@ public struct TextToEmoji {
     public func emoji(for text: String) -> String? {
         let input = text.lowercased()
         let emoji = NSLocalizedString(input, bundle: Bundle.module, value: input, comment: input)
-        
         guard emoji != input else { return nil }
-        
         return emoji
     }
 }
