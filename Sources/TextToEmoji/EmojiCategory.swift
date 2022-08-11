875f1be4b162785213ca3ed405192f3ae12b56df
Wouter Wisse <wouter.wisse@persgroep.net>
2022-08-10 21:29:18 -0400

    Create EmojiCategory to make it possible to have a preference for a specific category

---
+++ Sources/TextToEmoji/EmojiCategory.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public enum EmojiCategory {
+    /// 😃 Smileys & People - Emojis for smileys, people, families, hand gestures, clothing and accessories.
+    case smileysAndPeople
+    /// 🐻 Animals & Nature - Emojis for animals, nature, and weather.
+    case animalsAndNature
+    /// 🍔 Food & Drink - Emojis for fruit, vegetables, meals, beverages and utensils.
+    case foodAndDrink
+    /// ⚽ Activity - Emojis for sports, music, the arts, hobbies and other activities.
+    case activity
+    /// 🚀 Travel & Places - Emojis for varied scenes, locations, buildings and modes of transport.
+    case travelAndPlaces
+    /// 💡 Objects - Emojis for household items, celebrations, stationery and miscellaneous objects.
+    case objects
+    /// 💕 Symbols - Heart emojis, clocks, arrows, signs and shapes.
+    case symbols
+    /// 🎌 Flags - Country flag emojis.
+    case flags
+}
