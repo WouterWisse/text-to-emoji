4bd153ce741cb1af59e01f6e05c0e1c416e9f17f
Wouter Wisse <wouter.wisse@persgroep.net>
2022-08-10 21:54:45 -0400

    Fetch localized string from correct table

--- Sources/TextToEmoji/TextToEmoji.swift
+++ Sources/TextToEmoji/TextToEmoji.swift
@@ -1,10 +1,34 @@
 import Foundation
 
 public struct TextToEmoji {
-    
-    public func emoji(for text: String) -> String? {
+    /**
+     Matches the given text with an emoji.
+     
+     The category passed in `highPriorityCategory` will be given a higher priorty when multiple
+     emoji's match the given `text`. For example: "Chicken" could match 🐔 and 🍗.
+     - Passing `.animalsAndNature` will return 🐔
+     - Passing `.foodAndDrink` will return 🍗
+     
+     - Parameters:
+        - text: The text that will be matched with an emoji
+        - highPriorityCategory: The category with which is most likely to have a match
+     
+     - Returns: An emoji when a match is found, otherwise nil.
+     */
+    public func emoji(
+        for text: String,
+        highPriorityCategory: EmojiCategory? = nil
+    ) -> String? {
         let input = text.lowercased()
-        let emoji = NSLocalizedString(input, bundle: Bundle.module, value: input, comment: input)
+        
+        let emoji = NSLocalizedString(
+            input,
+            tableName: highPriorityCategory?.stringsFileName,
+            bundle: Bundle.module,
+            value: input,
+            comment: input
+        )
+        
         guard emoji != input else { return nil }
         return emoji
     }
