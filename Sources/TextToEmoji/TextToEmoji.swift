7d245e1af619ac78f774696e6ebcbf5e6271b4f2
Wouter Wisse <wouter.wisse@persgroep.net>
2022-08-11 09:30:28 -0400

    Try preferred category first, otherwise fall back on default

--- Sources/TextToEmoji/TextToEmoji.swift
+++ Sources/TextToEmoji/TextToEmoji.swift
@@ -4,30 +4,40 @@ public struct TextToEmoji {
     /**
      Tries to match the given text with an emoji.
      
-     The category passed in `highPriorityCategory` will be given a higher priorty when multiple
+     The category passed in `preferredCategory` will be given a higher priorty when multiple
      emoji's match the given `text`. For example: "Chicken" could match 🐔 and 🍗.
      - Passing `.animalsAndNature` will return 🐔
      - Passing `.foodAndDrink` will return 🍗
      
      - Parameters:
         - text: The text that will be matched with an emoji
-        - highPriorityCategory: The category with which is most likely to have a match
+        - preferredCategory: The category with which is most likely to have a match
      
      - Returns: An emoji when a match is found, otherwise nil.
      */
     public func emoji(
         for text: String,
-        highPriorityCategory: EmojiCategory? = nil
+        preferredCategory: EmojiCategory? = nil
     ) -> String? {
         let input = text.lowercased()
         
-        let emoji = NSLocalizedString(
-            input,
-            tableName: highPriorityCategory?.stringsFileName,
-            bundle: Bundle.module,
-            value: input,
-            comment: input
-        )
+        var emoji: String?
+        
+        // First try the preferred category.
+        if let preferredCategory = preferredCategory {
+            emoji = NSLocalizedString(
+                input,
+                tableName: preferredCategory.stringsFileName,
+                bundle: Bundle.module,
+                value: input,
+                comment: input
+            )
+        }
+        
+        // Fall back on default Localizable.strings.
+        if emoji == nil {
+            emoji = NSLocalizedString(input, bundle: Bundle.module, value: input, comment: input)
+        }
         
         guard emoji != input else { return nil }
         return emoji
