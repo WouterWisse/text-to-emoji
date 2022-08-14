1e73d9d83deb43d2f3550b0fbf6396f6b3e57e77
Wouter Wisse <wouter.wisse@persgroep.net>
2022-08-14 15:38:08 -0400

    Create StringMatchScoreProvider
    StringMatchScoreProvider uses the Levenshtein Distance Equation to create a score based on the amount of edits that are needed to get from one word to the other.

---
+++ Sources/TextToEmoji/Utilities/StringMatchScoreProvider.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct StringMatchScoreProvider {
+    var provideScore: (_ text: String, _ possibleMatch: String) -> Int
+}
+
+extension StringMatchScoreProvider {
+    static let `default` = StringMatchScoreProvider(
+        provideScore: { text, possibleMatch in
+            // https://en.wikipedia.org/wiki/Levenshtein_distance
+            let empty = [Int](repeating: 0, count: possibleMatch.count)
+            var distance = [Int](0...possibleMatch.count)
+
+            for (i, characterA) in text.enumerated() {
+                var current = [i + 1] + empty
+                for (j, characterB) in possibleMatch.enumerated() {
+                    current[j + 1] = characterA == characterB
+                    ? distance[j]
+                    : min(distance[j], distance[j + 1], current[j]) + 1
+                }
+                distance = current
+            }
+            return distance.last!
+        }
+    )
+}
