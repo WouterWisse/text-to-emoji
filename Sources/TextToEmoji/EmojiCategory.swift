3cbe4e64e58d63439f6283648f01450603e789b3
Wouter Wisse <wouter.wisse@persgroep.net>
2022-08-15 14:10:54 -0400

    Wrap enums in TextToEmoji extension to provide some namespacing

--- Sources/TextToEmoji/EmojiCategory.swift
+++ Sources/TextToEmoji/EmojiCategory.swift
@@ -1,33 +1,35 @@
 import Foundation
 
-public enum EmojiCategory {
-    /// 😃 Smileys & People - Emojis for smileys, people, families, hand gestures, clothing and accessories.
-    case smileysAndPeople
-    /// 🐻 Animals & Nature - Emojis for animals, nature, and weather.
-    case animalsAndNature
-    /// 🍔 Food & Drink - Emojis for fruit, vegetables, meals, beverages and utensils.
-    case foodAndDrink
-    /// ⚽ Activity - Emojis for sports, music, the arts, hobbies and other activities.
-    case activity
-    /// 🚀 Travel & Places - Emojis for varied scenes, locations, buildings and modes of transport.
-    case travelAndPlaces
-    /// 💡 Objects - Emojis for household items, celebrations, stationery and miscellaneous objects.
-    case objects
-    /// 💕 Symbols - Heart emojis, clocks, arrows, signs and shapes.
-    case symbols
-    /// 🎌 Flags - Country flag emojis.
-    case flags
-    
-    var tableName: String {
-        switch self {
-        case .smileysAndPeople: return "SmileysAndPeople"
-        case .animalsAndNature:  return "AnimalsAndNature"
-        case .foodAndDrink:  return "FoodAndDrink"
-        case .activity:  return "Activity"
-        case .travelAndPlaces:  return "TravelAndPlaces"
-        case .objects:  return "Objects"
-        case .symbols:  return "Symbols"
-        case .flags:  return "Flags"
+extension TextToEmoji {
+    public enum EmojiCategory: Equatable {
+        /// 😃 Smileys & People - Emojis for smileys, people, families, hand gestures, clothing and accessories.
+        case smileysAndPeople
+        /// 🐻 Animals & Nature - Emojis for animals, nature, and weather.
+        case animalsAndNature
+        /// 🍔 Food & Drink - Emojis for fruit, vegetables, meals, beverages and utensils.
+        case foodAndDrink
+        /// ⚽ Activity - Emojis for sports, music, the arts, hobbies and other activities.
+        case activity
+        /// 🚀 Travel & Places - Emojis for varied scenes, locations, buildings and modes of transport.
+        case travelAndPlaces
+        /// 💡 Objects - Emojis for household items, celebrations, stationery and miscellaneous objects.
+        case objects
+        /// 💕 Symbols - Heart emojis, clocks, arrows, signs and shapes.
+        case symbols
+        /// 🎌 Flags - Country flag emojis.
+        case flags
+        
+        var tableName: String {
+            switch self {
+            case .smileysAndPeople: return "SmileysAndPeople"
+            case .animalsAndNature:  return "AnimalsAndNature"
+            case .foodAndDrink:  return "FoodAndDrink"
+            case .activity:  return "Activity"
+            case .travelAndPlaces:  return "TravelAndPlaces"
+            case .objects:  return "Objects"
+            case .symbols:  return "Symbols"
+            case .flags:  return "Flags"
+            }
         }
     }
 }
