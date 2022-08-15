248df12f18fce32a075fe903063cdb26463d3158
Wouter Wisse <wouter.wisse@persgroep.net>
2022-08-15 09:28:15 -0400

    Rename to MatchAccuracy

---
+++ Sources/TextToEmoji/MatchAccuracy.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public enum MatchAccuracy {
+    /// String must match at least 50%. Not very accurate.
+    case low
+    /// String must match at least 70%. Accurate.
+    case medium
+    /// String must match at least 80%. Very accurate.
+    case high
+    /// String must match 100%. Most accurate.
+    case perfect
+    /// Custom value from `0.0 - 1.0`. Higher number leaves less room for error, thus more accurate results.
+    case custom(value: Double)
+    
+    /// Percentage based accuracy means that more differences are allowed in longer Strings.
+    var percentage: Double {
+        switch self {
+        case .low:
+            return 0.5
+        case .medium:
+            return 0.7
+        case .high:
+            return 0.8
+        case .perfect:
+            return 1.0
+        case .custom(let value):
+            return value
+        }
+    }
+}
