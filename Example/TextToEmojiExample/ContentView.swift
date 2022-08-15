9de807693f8d6733a2aa90c1ade3b2bfba30aa32
Wouter Wisse <wouter.wisse@persgroep.net>
2022-08-15 13:14:27 -0400

    Create example project

---
+++ Example/TextToEmojiExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextToEmojiExample
+//
+//  Created by Wouter Wisse on 8/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/TextToEmojiExample/TextToEmojiExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToEmojiExampleApp.swift
+//  TextToEmojiExample
+//
+//  Created by Wouter Wisse on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct TextToEmojiExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
