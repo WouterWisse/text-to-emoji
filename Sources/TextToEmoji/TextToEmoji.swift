8d32dbf90ba92d2565c4c1bd5d50104dc84f736a
Wouter Wisse <wouter.wisse@persgroep.net>
2022-10-14 15:17:59 -0400

    Return emoji for direct match

--- Sources/TextToEmoji/TextToEmoji.swift
+++ Sources/TextToEmoji/TextToEmoji.swift
@@ -48,17 +48,33 @@ private extension TextToEmoji {
         return try await emojiTask.value
     }
     
-    static  func emoji(for text: String, from tableName: String) -> String? {
+    static func emoji(
+        for text: String,
+        from tableName: String
+    ) -> String? {
+        let bundle = Bundle.module
         guard
-            let path = Bundle.module.path(forResource: tableName, ofType: "strings"),
+            let path = bundle.path(forResource: tableName, ofType: "strings"),
             let dictionary = NSDictionary(contentsOfFile: path),
             let allKeys = dictionary.allKeys as? [String]
         else { return nil  }
         
+        // Return an exact match immediately.
+        if let exactMatch = allKeys.first(where: { $0 == text }) {
+            return NSLocalizedString(
+                exactMatch,
+                tableName: tableName,
+                bundle: bundle,
+                comment: exactMatch
+            )
+        }
+        
+        // Use Levenshtein Distance to get best match.
         let scores = allKeys
             .map { (key: $0, score: StringMatchScoreProvider.default.provideScore(text, $0)) }
             .sorted(by: { $0.score < $1.score })
         
+        // Best match needs to be at least 80% correct.
         guard
             let bestMatch = scores.first,
             Double(bestMatch.score) <= Double(bestMatch.key.count) * 0.2
@@ -67,7 +83,7 @@ private extension TextToEmoji {
         return NSLocalizedString(
             bestMatch.key,
             tableName: tableName,
-            bundle: Bundle.module,
+            bundle: bundle,
             comment: bestMatch.key
         )
     }
