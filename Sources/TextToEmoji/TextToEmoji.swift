1162f3eeaae0df015b7c7b9fe20b2d95a6cab53d
Wouter Wisse <wouter.wisse@persgroep.net>
2022-10-17 12:11:32 -0400

    Check for all preferred languages

--- Sources/TextToEmoji/TextToEmoji.swift
+++ Sources/TextToEmoji/TextToEmoji.swift
@@ -53,38 +53,59 @@ private extension TextToEmoji {
         from tableName: String
     ) -> String? {
         let bundle = Bundle.module
-        guard
-            let path = bundle.path(forResource: tableName, ofType: "strings"),
-            let dictionary = NSDictionary(contentsOfFile: path),
-            let allKeys = dictionary.allKeys as? [String]
-        else { return nil  }
+        let preferredLanguages = availablePreferredLanguages(in: bundle)
         
-        // Return an exact match immediately.
-        if let exactMatch = allKeys.first(where: { $0 == text }) {
-            return NSLocalizedString(
-                exactMatch,
-                tableName: tableName,
-                bundle: bundle,
-                comment: exactMatch
-            )
+        /*
+         Many iOS users have their device language set to English, but still have their native language in
+         the list of preferred languages. To get the best possible result, we check for all those preferred languages
+         as well (if they are supported by this package).
+         */
+        for language in preferredLanguages {
+            guard
+                let path = bundle.localizedTablePath(for: tableName, localization: language),
+                let dictionary = NSDictionary(contentsOfFile: path),
+                let allKeys = dictionary.allKeys as? [String]
+            else { continue }
+            
+            // Return an exact match immediately.
+            if let exactMatch = allKeys.first(where: { $0 == text }) {
+                return dictionary[exactMatch] as? String
+            }
+            
+            // Use Levenshtein Distance to get best match.
+            let scores = allKeys
+                .map { (key: $0, score: StringMatchScoreProvider.default.provideScore(text, $0)) }
+                .sorted(by: { $0.score < $1.score })
+            
+            // Best match needs to be at least 80% correct.
+            guard
+                let bestMatch = scores.first,
+                Double(bestMatch.score) <= Double(bestMatch.key.count) * 0.2
+            else { continue }
+            
+            return dictionary[bestMatch.key] as? String
         }
         
-        // Use Levenshtein Distance to get best match.
-        let scores = allKeys
-            .map { (key: $0, score: StringMatchScoreProvider.default.provideScore(text, $0)) }
-            .sorted(by: { $0.score < $1.score })
-        
-        // Best match needs to be at least 80% correct.
-        guard
-            let bestMatch = scores.first,
-            Double(bestMatch.score) <= Double(bestMatch.key.count) * 0.2
-        else { return nil }
-        
-        return NSLocalizedString(
-            bestMatch.key,
-            tableName: tableName,
-            bundle: bundle,
-            comment: bestMatch.key
+        return nil
+    }
+    
+    static func availablePreferredLanguages(in bundle: Bundle) -> [String] {
+        let availableLocalizations = bundle.localizations
+        let preferredLanguages = Locale.preferredLanguages.compactMap { $0.components(separatedBy: "-").first }
+        let availablePreferredLanguages = preferredLanguages.filter { availableLocalizations.contains($0) }
+        return availablePreferredLanguages
+    }
+}
+
+// MARK: Table for Localization
+
+private extension Bundle {
+    func localizedTablePath(for tableName: String, localization: String) -> String? {
+        path(
+            forResource: tableName,
+            ofType: "strings",
+            inDirectory: "\(localization).lproj",
+            forLocalization: localization
         )
     }
 }
